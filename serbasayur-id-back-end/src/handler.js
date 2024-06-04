d326fc5e0a3c665aed3c9c9db7ad6f2055ebc81c
fauzanrizkyanag <fauzanrizkyana@gmail.com>
2024-06-04 14:47:43 +0700

    Fix CRUD

--- serbasayur-id-back-end/src/handler.js
+++ serbasayur-id-back-end/src/handler.js
@@ -1,9 +1,19 @@
-const { nanoid } = require("nanoid");
-const products = require("./products");
-const db = require("./db_config");
+const { nanoid } = require('nanoid');
+const db = require('./db_config');
 
 async function getAllProducts(callback) {
-  const sql = "SELECT * FROM products";
+  const sql = 'SELECT * FROM products';
+
+  db.query(sql, (err, results) => {
+    if (err) {
+      throw err;
+    }
+    return callback(Object.values(JSON.parse(JSON.stringify(results))));
+  });
+}
+
+async function getProductById(idProduk, callback) {
+  const sql = `SELECT * FROM products WHERE id_produk='${idProduk}'`;
 
   db.query(sql, (err, results) => {
     if (err) {
@@ -14,7 +24,9 @@ async function getAllProducts(callback) {
 }
 
 const addProductHandler = (request, h) => {
-  const { nama, deskripsi, harga, image, kuantitas, rating } = request.payload;
+  const {
+    nama, deskripsi, harga, image, kuantitas, rating,
+  } = request.payload;
 
   const idProduk = `product-${nanoid(16)}`;
 
@@ -24,8 +36,8 @@ const addProductHandler = (request, h) => {
     db.query(sql);
 
     const response = h.response({
-      status: "success",
-      message: "Berhasil",
+      status: 'success',
+      message: 'Berhasil',
       data: {
         idProduk,
       },
@@ -34,7 +46,7 @@ const addProductHandler = (request, h) => {
     return response;
   } catch (error) {
     const response = h.response({
-      status: "fail",
+      status: 'fail',
       message: error,
     });
     response.code(500);
@@ -47,11 +59,10 @@ const getAllProductsHandler = async () => {
     getAllProducts((results) => {
       const productList = [];
       Object.keys(results).forEach((v) => {
-        console.log(results[v]);
         productList.push(results[v]);
       });
       const response = {
-        status: "success",
+        status: 'success',
         data: {
           products: productList,
         },
@@ -65,80 +76,91 @@ const getAllProductsHandler = async () => {
 const getProductByIdHandler = (request, h) => {
   const { idProduk } = request.params;
 
-  const product = products.filter((p) => p.idProduk === idProduk)[0];
-
-  if (product !== undefined) {
-    return {
-      status: "success",
-      data: {
-        product,
-      },
-    };
-  }
-
-  const response = h.response({
-    status: "fail",
-    message: "Produk tidak ditemukan",
+  const promise = new Promise((resolve) => {
+    getProductById(idProduk, (results) => {
+      if (typeof results !== 'undefined' && results.length > 0) {
+        const response = {
+          status: 'success',
+          data: {
+            product: results[0],
+          },
+        };
+        resolve(response);
+      } else {
+        const response = h.response({
+          status: 'fail',
+          message: 'Produk tidak ditemukan',
+        });
+        response.code(404);
+        resolve(response);
+      }
+    });
   });
-  response.code(404);
-  return response;
+  return promise;
 };
 
 const editProductByIdHandler = (request, h) => {
   const { idProduk } = request.params;
 
-  const { nama, deskripsi, harga, image, kuantitas, rating } = request.payload;
+  const {
+    nama, deskripsi, harga, image, kuantitas, rating,
+  } = request.payload;
 
-  const index = products.findIndex((product) => product.idProduk === idProduk);
+  const promise = new Promise((resolve) => {
+    getProductById(idProduk, (results) => {
+      if (typeof results !== 'undefined' && results.length > 0) {
+        const sql = `UPDATE products SET nama='${nama}',deskripsi='${deskripsi}',harga=${harga},image='${image}',kuantitas=${kuantitas},rating=${rating} WHERE id_produk='${idProduk}'`;
 
-  if (index !== -1) {
-    products[index] = {
-      ...products[index],
-      nama,
-      deskripsi,
-      harga,
-      image,
-      kuantitas,
-      rating,
-    };
+        db.query(sql);
 
-    const response = h.response({
-      status: "success",
-      message: "Produk berhasil diperbarui",
+        const response = h.response({
+          status: 'success',
+          message: 'Produk berhasil diperbarui',
+        });
+        response.code(200);
+        resolve(response);
+      } else {
+        const response = h.response({
+          status: 'fail',
+          message: 'Gagal memperbarui produk. Id tidak ditemukan',
+        });
+        response.code(404);
+        resolve(response);
+      }
     });
-    response.code(200);
-    return response;
-  }
-
-  const response = h.response({
-    status: "fail",
-    message: "Gagal memperbarui produk. Id tidak ditemukan",
   });
-  response.code(404);
-  return response;
+
+  return promise;
 };
 
 const deleteProductByIdHandler = (request, h) => {
   const { idProduk } = request.params;
 
-  const index = products.findIndex((product) => product.idProduk === idProduk);
+  const promise = new Promise((resolve) => {
+    getProductById(idProduk, (results) => {
+      if (typeof results !== 'undefined' && results.length > 0) {
+        const sql = `DELETE FROM products WHERE id_produk='${idProduk}'`;
 
-  if (index !== -1) {
-    products.splice(index, 1);
-    const response = h.response({
-      status: "success",
-      message: "Produk berhasil dihapus",
+        db.query(sql);
+
+        const response = h.response({
+          status: 'success',
+          message: 'Produk berhasil dihapus',
+        });
+        response.code(200);
+        resolve(response);
+      } else {
+        const response = h.response({
+          status: 'fail',
+          message: 'Produk gagal dihapus. Id tidak ditemukan',
+        });
+        response.code(404);
+        resolve(response);
+      }
     });
-    response.code(200);
-    return response;
-  }
-
-  const response = h.response({
-    status: "fail",
-    message: "Produk gagal dihapus. Id tidak ditemukan",
   });
-  response.code(404);
-  return response;
+
+  return promise;
 };
 
 module.exports = {
