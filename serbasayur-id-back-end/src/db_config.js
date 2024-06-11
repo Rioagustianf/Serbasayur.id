10ec4fc1e884e40d5b570f755f2820ca6cb1ab2d
fauzanrizkyanag <fauzanrizkyana@gmail.com>
2024-06-12 00:48:08 +0700

    Update Back-end

--- serbasayur-id-back-end/src/db_config.js
+++ serbasayur-id-back-end/src/db_config.js
@@ -1,10 +1,15 @@
 const mysql = require('mysql');
+require('dotenv').config();
+
+const {
+  DB_HOST, DB_USER, DB_PASS, DB_NAME,
+} = process.env;
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'serbasayur_id',
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASS,
+  database: DB_NAME,
 });
 
 db.connect((err) => {
