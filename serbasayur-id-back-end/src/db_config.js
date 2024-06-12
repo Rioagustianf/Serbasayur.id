a67387b8e3dabc5f7555db3c22a6172d3d0fb0e0
fauzanrizkyanag <fauzanrizkyana@gmail.com>
2024-06-12 13:42:10 +0700

    Fix DB Cofigurations

--- serbasayur-id-back-end/src/db_config.js
+++ serbasayur-id-back-end/src/db_config.js
@@ -2,11 +2,12 @@ const mysql = require('mysql');
 require('dotenv').config();
 
 const {
-  DB_HOST, DB_USER, DB_PASS, DB_NAME,
+  DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME,
 } = process.env;
 
 const db = mysql.createConnection({
   host: DB_HOST,
+  port: (DB_PORT || 3306),
   user: DB_USER,
   password: DB_PASS,
   database: DB_NAME,
