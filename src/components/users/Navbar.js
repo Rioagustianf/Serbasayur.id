3a98141deb8209a9aa2bde2da5d23ea64a5f1680
Rio Agustian <rioagustian2001@gmail.com>
2024-06-19 17:37:03 +0700

    updaet

--- src/components/users/Navbar.js
+++ src/components/users/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = {
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
           <div class="container-fluid">
             <div class="logo">
-              <a href="/"><img height="50px" src="../../public/images/logo1.png" alt="Serba Sayur ID"></a>
+              <a href="/"><img height="50px" src="../public/images/logo1.png" alt="Serba Sayur ID"></a>
             </div>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
